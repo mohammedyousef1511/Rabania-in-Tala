9a9ae73d098d1548a2b4f3848da3b381fdf0cf8e
mohammedyousef1511 <mohammedabodeeb1511@gmail.com>
2025-04-21 08:27:09 +0200

    fix: add base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // ✅ السطر المهم لحل مشكلة الصفحة الفاضية على Vercel
   plugins: [react()],
   resolve: {
     alias: {
